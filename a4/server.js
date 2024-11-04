e43053a4a5a69f03514210d19df29b13e87ad7e6
hansol nam <hansolnam@hansols-mbp-2.home>
2024-11-04 17:27:57 -0500

    app.use(express.static(path.join(__dirname, 'public')));

--- a4/server.js
+++ a4/server.js
@@ -14,6 +14,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 
 // Initialize LEGO data 
 legoData.initialize()
